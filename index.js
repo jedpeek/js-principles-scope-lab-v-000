95dea47d811d5a3d98b5a55b8fffd703edbb2f52
Jed Peek <jedpeek@yahoo.com>
2018-08-31 22:19:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Write your solution in this file!
 customerName = 'bob';
 
-function upperCaseCustomerName(){
-  return customerName.toUpperCase()
-};
+
 
 function setBestCustomer(){
   return `not ${customerName}`
